3458b64ddfdab2445ddaf708326a27dde6a941b5
TemirbolatMaratuly <snake2009003@mail.ru>
2022-03-25 13:26:16 +0600

    Initial commit V1.1

--- settings/conf.py
+++ settings/conf.py
@@ -2,3 +2,8 @@
 # SECRET KEY VALUE
 #
 SECRET_KEY = 'hb@==g3%tn-qt#l@salpc7nbu9w9a0#q6s2vd+#61lzx9ew&$n'
+
+# --------------------------------------------------------------
+# ADMIN_SITE_URL
+#
+ADMIN_SITE_URL = 'custom_admin/'
--- urls/urls.py
+++ urls/urls.py
@@ -18,8 +18,17 @@ from django.urls import (
     path,
     include,
 )
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_SITE_URL, admin.site.urls),
     path('', include('shortner.urls')),
-]
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+) + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
